8f3bc67d0e0a704c2e581be5fb24c665d2dbf987
ksrimmy <flo@trablo.net>
2024-07-10 11:29:02 +0200

    added crash detection

--- remote_car.py
+++ remote_car.py
@@ -1,7 +1,8 @@
-from pybricks.parameters import Direction, Port, Stop
+from pybricks.parameters import Direction, Port, Color
 from pybricks.pupdevices import Motor, UltrasonicSensor
 from pybricks.robotics import Car
-from pybricks.tools import wait
+from pybricks.tools import wait, StopWatch
+from pybricks.hubs import PrimeHub
 
 # Standard MicroPython modules
 from usys import stdin, stdout
@@ -9,19 +10,23 @@ from uselect import poll
 
 
 # Set up all devices.
+distanceSensor = UltrasonicSensor(Port.A)
 steering = Motor(Port.B, Direction.COUNTERCLOCKWISE)
 motorL = Motor(Port.C, Direction.COUNTERCLOCKWISE)
 motorR = Motor(Port.D, Direction.CLOCKWISE)
 
+hub = PrimeHub()
+
+
 # Lower the acceleration so the car starts and stops realistically.
-motorL.control.limits(acceleration=500)
-motorR.control.limits(acceleration=500)
+motorL.control.limits(acceleration=200)
+motorR.control.limits(acceleration=200)
 
 
 car = Car(steering, [motorL, motorR])
+# car = Car(steering, [motorL, motorR], torque_limit=40)
 
 
-distanceSensor = UltrasonicSensor(Port.A)
 # distanceSensor.lights.on()
 
 
@@ -33,28 +38,52 @@ keyboard.register(stdin)
 stdout.buffer.write(b"rdy")
 data = ""
 cmd_arr = []
+collision_counter = -1
+CAR_LENGTH = 130
+stopWatch = StopWatch()
+last_crash_time = 0
+
 while True:
     # Let the remote program know we are ready for a command.
-  
-
-    # Optional: Check available input.
-    # while not keyboard.poll(0):
-    #     # Optional: Do something here.
-    #     wait(10)
+    full_speed_mode = False
+    cur_dist = distanceSensor.distance()
+    if cur_dist < 50:
+        full_speed_mode = True
+    else:
+        if cur_dist < CAR_LENGTH:
+            if not new_crash:
+                new_crash = True
+                crash_time = stopWatch.time()
+                if crash_time - last_crash_time > 1000:
+                    collision_counter += 1
+                last_crash_time = crash_time
+        elif cur_dist >= CAR_LENGTH:
+            new_crash = False
+            
+            
+ 
+        if collision_counter >= 0:
+            hub.display.pixel(collision_counter/4, collision_counter%4, brightness=100)
 
     if keyboard.poll(0):
         #cmd = input()
         data += stdin.read(1)
-    
+         
         if data[-1] == "\n":
             cmd_arr = data[0:-1].split("|")
             data = ""
 
-            # Control steering using the left - and + buttons.
-            car.steer(50 if "lft" in cmd_arr else (-50 if "rgt" in cmd_arr else 0))
-            # Control drive power using the right - and + buttons.
-            car.drive_power(80 if "fwd" in cmd_arr else (-80 if "rev" in cmd_arr else 0))
-    
+            if full_speed_mode:
+                # Control steering using the left - and + buttons.
+                car.steer(100 if "lft" in cmd_arr else (-100 if "rgt" in cmd_arr else 0))
+                # Control drive power using the right - and + buttons.
+                car.drive_power(100 if "fwd" in cmd_arr else (-100 if "rev" in cmd_arr else 0))
+            else:
+                # Control steering using the left - and + buttons.
+                car.steer(60 if "lft" in cmd_arr else (-60 if "rgt" in cmd_arr else 0))
+                # Control drive power using the right - and + buttons.
+                car.drive_power(60 if "fwd" in cmd_arr else (-60 if "rev" in cmd_arr else 0))
+        
     # stdout.buffer.write(data)
     wait(2)
     
